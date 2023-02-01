3d0470449d50185c2cbfec42f6d6469ea7442789
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-02-01 10:38:32 -0600

    Add local_screen_and_web config

--- chimerapy_orchestrator/nodes/nodes.py
+++ chimerapy_orchestrator/nodes/nodes.py
@@ -1,8 +1,12 @@
+import platform
 import time
 from typing import Any, Dict
 
 import chimerapy as cp
 import cv2
+import imutils
+import numpy as np
+from PIL import ImageGrab
 
 from chimerapy_orchestrator.utils import register_chimerapy_node
 
@@ -39,3 +43,39 @@ class ShowWindow(cp.Node):
 
             cv2.imshow(name, data_chunk.get("frame")["value"])
             cv2.waitKey(1)
+
+
+@register_chimerapy_node
+class ScreenCaptureNode(cp.Node):
+    def __init__(self):
+        super(ScreenCaptureNode, self).__init__(name="ScreenCaptureNode")
+
+    def prep(self):
+        if platform.system() == "Windows":
+            import dxcam
+
+            self.camera = dxcam.create()
+        else:
+            self.camera = None
+
+    def step(self):
+        # Noticed that screencapture methods highly depend on OS
+        if platform.system() == "Windows":
+            time.sleep(1 / 30)
+            frame = self.camera.grab()
+            if not isinstance(frame, np.ndarray):
+                return None
+            else:
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        else:
+            frame = cv2.cvtColor(
+                np.array(ImageGrab.grab(), dtype=np.uint8), cv2.COLOR_RGB2BGR
+            )
+
+        # Save the frame and package it
+        self.save_video(name="screen", data=frame, fps=20)
+        data_chunk = cp.DataChunk()
+        data_chunk.add("frame", imutils.resize(frame, width=720), "image")
+
+        # Then send it
+        return data_chunk
