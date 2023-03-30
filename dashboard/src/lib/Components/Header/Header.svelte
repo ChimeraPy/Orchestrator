dc8fa4fcaf9d67d3947bde57af9fb0a9139d81ba
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-03-30 10:07:12 -0500

    Don't omit lib directory in dashboard. Closes #34 (#35)

---
+++ dashboard/src/lib/Components/Header/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+	export let sections: { string: string }[], logo;
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img src={logo} class="mr-3 h-6 sm:h-9" alt="ChimeraPy" />
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		{#each sections as section, i}
+			<NavLi
+				href={section.slug}
+				active={$page.url.pathname === section.slug}
+				class="hover:text-blue-600">{section.title}</NavLi
+			>
+		{/each}
+	</NavUl>
+</Navbar>
